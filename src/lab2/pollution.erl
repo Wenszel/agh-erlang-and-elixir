af3df7952d053d313a7a6540796d0afc34319803
Wenszel <wiktor.smaga13@gmail.com>
2024-03-21 12:44:58 +0100

    Changed monitor record from a list to a pair of maps
    This change speeds up the process of checking if a station exists from linear to constant time

--- src/lab2/pollution.erl
+++ src/lab2/pollution.erl
@@ -7,116 +7,128 @@
 %%% Created : 18. mar 2024 11:41 AM
 %%%-------------------------------------------------------------------
 -module(pollution).
+-feature(maybe_expr,enable).
 -author("wiktorsmaga").
--record(monitor, {name, coordinates, measurements}).
+
+-record(monitor, {coordinates_to_name, name_to_measurements}).
 -record(measurement, {type, value, time}).
 
 -export([create_monitor/0, add_station/3, add_value/5, remove_value/4, get_one_value/4, get_station_mean/3, get_daily_mean/3]).
 
-create_monitor() -> [].
+create_monitor() ->  #monitor{coordinates_to_name = #{}, name_to_measurements = #{}}.
 
-add_station(NewName, NewCoords, Monitor) ->
-  case lists:any(
-    fun(#monitor{name = Name, coordinates = Coords}) ->
-      NewName == Name orelse Coords == NewCoords end, Monitor)
-  of
-    true -> {error, "Station with this name already exists"};
-    false ->
-      lists:append(Monitor, [#monitor{name = NewName, coordinates = NewCoords, measurements = []}])
+add_station(Name, Coords, Monitor) ->
+  IsName = maps:is_key(Name, Monitor#monitor.name_to_measurements),
+  IsCoords = maps:is_key(Coords, Monitor#monitor.coordinates_to_name),
+  case IsName orelse IsCoords of
+    true -> {error, "already exisits"};
+    false -> #monitor{
+      name_to_measurements = maps:put(Name,[], Monitor#monitor.name_to_measurements),
+      coordinates_to_name = maps:put(Coords, Name, Monitor#monitor.coordinates_to_name)
+    }
   end.
 
-contains_measurement(Type, Time, [{_, Type, Value, Time} | _]) -> {measurement, Type, Value, Time};
+handle_coordinates({Long, Lat}, Function, Monitor) ->
+  case maps:find({Long, Lat}, Monitor#monitor.coordinates_to_name) of
+    {ok, Name} -> Function(Name);
+    error -> {error, "Station not found"}
+  end.
+
+contains_measurement(Type, Time, [#measurement{type=Type, time=Time} | _]) -> true;
 contains_measurement(Type, Time, [_ | T]) -> contains_measurement(Type, Time, T);
 contains_measurement(_, _, []) -> false.
 
-add_value(NameOrCoords, Time, Type, Value, Monitor) ->
-  IsMember = lists:any(fun(#monitor{name = Name, coordinates = Coords, measurements = Measurements}) ->
-    (Name == NameOrCoords orelse Coords == NameOrCoords)
-      andalso contains_measurement(Type, Time, Measurements) == false
-                       end, Monitor),
-  case IsMember of
-    false -> {error, "Bad args"};
-    _ ->
-          lists:map(
-          fun
-            (#monitor{name = Name, coordinates = Coords, measurements = Measurements}) when NameOrCoords == Name orelse NameOrCoords == Coords ->
-              #monitor{name = Name, coordinates = Coords, measurements = lists:append([#measurement{type = Type, value = Value, time = Time}], Measurements)};
-              (M) -> M
-          end
-          , Monitor)
+get_measurement(Type, Time, [#measurement{type=Type, time=Time, value=Value} | _]) -> Value;
+get_measurement(Type, Time, [_|T]) -> get_measurement(Type, Time, T);
+get_measurement(_, _, []) -> {error, "Measurement not found"}.
+
+append_measurements(Name, Measurement, Measurements, Monitor) ->
+  #monitor {
+    name_to_measurements = maps:put(Name, [
+      #measurement{
+        time = Measurement#measurement.time,
+        type = Measurement#measurement.type,
+        value = Measurement#measurement.value
+    } ] ++ Measurements, Monitor#monitor.name_to_measurements),
+    coordinates_to_name = Monitor#monitor.coordinates_to_name
+  }.
+
+add_value({Long, Lat}, Time, Type, Value, Monitor) ->
+  handle_coordinates({Long, Lat}, fun (Name) -> add_value(Name, Time, Type, Value, Monitor) end, Monitor);
+add_value(Name, Time, Type, Value, Monitor) ->
+  maybe
+    {ok, Measurements} ?= maps:find(Name, Monitor#monitor.name_to_measurements),
+    false ?= contains_measurement(Type, Time, Measurements),
+    append_measurements(Name, #measurement{ time = Time, type = Type, value = Value}, Measurements, Monitor)
+  else
+    error -> {error, "Station not found"};
+    true -> {error, "Measurement already exists"}
   end.
 
-remove_value(NameOrCoords, Time, Type, Monitor) ->
-  IsMember = lists:any(fun(#monitor{name = Name, coordinates = Coords, measurements = Measurements}) ->
-    (Name == NameOrCoords orelse Coords == NameOrCoords) andalso
-      not (contains_measurement(Type, Time, Measurements) == false)
-                       end, Monitor),
-  case IsMember of
-    true ->
-      lists:map(
-        fun(#monitor{name = Name, coordinates = Coords, measurements = Measurements})
-          when NameOrCoords == Name orelse NameOrCoords == Coords ->
-          #monitor{name = Name, coordinates = Coords, measurements = lists:delete(contains_measurement(Type, Time, Measurements), Measurements)}
-        end
-        , Monitor);
-    false -> {error, "Bad args"}
+find_measurement(Time, Type, [#measurement{type=Type, time=Time, value=Value} | _]) ->
+  #measurement{type=Type, time=Time, value=Value};
+find_measurement(SearchedTime, SearchedType, [#measurement{type=_, time=_} | T]) ->
+  find_measurement(SearchedTime, SearchedType, T);
+find_measurement(_, _, []) -> false.
+
+remove_measurement(Time, Type, Measurements) ->
+    lists:delete(find_measurement(Time, Type, Measurements), Measurements).
+
+remove_value({Long, Lat}, Time, Type, Monitor) ->
+  handle_coordinates({Long, Lat}, fun(Name) -> remove_value(Name, Time, Type, Monitor) end, Monitor);
+remove_value(Name, Time, Type, Monitor) ->
+  maybe
+    {ok, Measurements} ?= maps:find(Name, Monitor#monitor.name_to_measurements),
+    true ?= contains_measurement(Type, Time, Measurements),
+    #monitor {
+      name_to_measurements = maps:put(Name, remove_measurement(Time, Type, Measurements), Monitor#monitor.name_to_measurements),
+      coordinates_to_name = Monitor#monitor.coordinates_to_name
+    }
+  else
+    error -> {error, "Station not found"};
+    false -> {error, "Measurement not found"}
   end.
 
-get_value_from_measurements(_, _, []) -> {error, "no"};
-get_value_from_measurements(Type, Time, [#measurement{type=Type, time=Time, value=Value} | _])
-  -> Value;
-get_value_from_measurements(Type, Time, [#measurement{type=_Type, time=_Time} | T])
-  -> get_value_from_measurements(Type, Time, T).
-
-get_one_value(_, _, _, []) -> {error, "no"};
-get_one_value(NameOrCoords, Time, Type, [#monitor{name=Name, coordinates=Coords, measurements = Measurements} | _])
-  when NameOrCoords == Name orelse NameOrCoords == Coords
-  ->
-  get_value_from_measurements(Type, Time, Measurements);
-get_one_value(NoC, Time, Type, [_|T]) -> get_one_value(NoC, Time, Type, T).
-
-get_station_mean(_, _, []) -> {error, "no"};
-get_station_mean(NameOrCoords, SearchedType, [#monitor{name=Name, coordinates=Coords, measurements = Measurements} | _])
-  when NameOrCoords == Name orelse NameOrCoords == Coords
-  ->
-    Count = lists:foldl(fun
-                          (#measurement{type=Type}, Acc) when SearchedType == Type -> Acc + 1;
-                          (_, Acc) -> Acc
-                        end, 0, Measurements),
-    if
-      Count > 0 -> lists:foldl(fun
-                                 (#measurement{type=Type, value=Value}, Acc) when Type == SearchedType -> Acc + Value;
-                                 (_, Acc) -> Acc
-                               end, 0, Measurements) / Count;
-      true -> {error, "no"}
-    end;
-get_station_mean(NoC, Type, [_|T]) -> get_station_mean(NoC, Type, T).
-
-is_measurement_on_given_day(Type, {Y, M, D}, [{_, Type, Value, {{Y, M, D}, _}} | _]) -> Value;
-is_measurement_on_given_day(Type, Date, [_ | T]) -> is_measurement_on_given_day(Type, Date, T);
-is_measurement_on_given_day(_, _, []) -> false.
-
-
-get_number_of_measurements(_, _, Acc, []) -> Acc;
-get_number_of_measurements(Type, Date, Acc, [#monitor{measurements = M} |T]) ->
-  IsMember = is_measurement_on_given_day(Type, Date, M),
-  case IsMember of
-    false -> get_number_of_measurements(Type, Date, Acc, T);
-    _ -> get_number_of_measurements(Type, Date, Acc + 1, T)
+get_one_value({Long, Lat}, Time, Type, Monitor) ->
+  handle_coordinates({Long, Lat}, fun (Name) -> get_one_value(Name, Time, Type, Monitor) end, Monitor);
+get_one_value(Name, Time, Type, Monitor) ->
+  maybe
+    {ok, Measurements} ?= maps:find(Name, Monitor#monitor.name_to_measurements),
+    Value ?= get_measurement(Type, Time, Measurements),
+    Value
+  else
+    error -> {error, "Station not found"};
+    {error, Message} -> {error, Message}
   end.
 
-sum_measurements(_, _, Acc, []) -> Acc;
-sum_measurements(Type, Date, Acc, [#monitor{measurements = M} |T]) ->
-  IsMember = is_measurement_on_given_day(Type, Date, M),
-  case IsMember of
-    false -> sum_measurements(Type, Date, Acc, T);
-    Value -> sum_measurements(Type, Date, Acc + Value, T)
+safe_div(X, Y) when Y /= 0 -> X / Y;
+safe_div(_, _) -> {error, "Div by 0"}.
+
+get_station_mean({Long, Lat}, Type, Monitor) ->
+  handle_coordinates({Long, Lat}, fun(Name) -> get_station_mean(Name, Type, Monitor) end, Monitor);
+get_station_mean(Name, SearchedType, Monitor) ->
+  maybe
+    {ok, Measurements} ?= maps:find(Name, Monitor#monitor.name_to_measurements),
+    {Sum, Count} = lists:foldl(fun
+                        (#measurement{type=Type, value=Value}, {S, C})
+                          when Type == SearchedType -> {S + Value, C+1};
+                          (_, Acc)-> Acc end, {0, 0}, Measurements),
+    safe_div(Sum, Count)
+  else
+    error -> {error, "Station not found"};
+    {error, Message} -> {error, Message}
   end.
 
 get_daily_mean(Type, Date, Monitor) ->
-  Count = get_number_of_measurements(Type, Date, 0, Monitor),
-  Sum = sum_measurements(Type, Date, 0, Monitor),
-  if
-    Count == 0 -> {error, "No Elements"};
-    true -> Sum / Count
-  end.
+  {Sum, Count} = maps:fold(fun(_, Measurements, {Sum, Count}) ->
+    {PartSum, PartCount} = lists:foldl(fun
+                                         (M, {AccSum, AccCount})->
+                                           {MDate, _} = M#measurement.time,
+                                           case M#measurement.type == Type andalso MDate == Date of
+                                             true -> {AccSum + M#measurement.value, AccCount + 1};
+                                               false -> {AccSum, AccCount}
+                                           end
+                                        end, {0, 0}, Measurements),
+    {Sum + PartSum, Count + PartCount}
+                            end, {0, 0}, Monitor#monitor.name_to_measurements),
+  safe_div(Sum, Count).
